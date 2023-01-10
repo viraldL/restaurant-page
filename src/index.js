126f66eb596befd46a7fab942552b7536535c582
viraldL <107942715+viraldL@users.noreply.github.com>
2023-01-10 20:50:18 +0100

    Make the first module for the main page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("esa")
+import { gsap } from "gsap";
+import mainLoad from "./mainPage.js";
+
+const homeBtn = document.querySelector("#homeBtn");
+
+homeBtn.addEventListener("click", () => {
+    console.log("work");
+    mainLoad();
+});
---
+++ src/mainPage.js
@@ -0,0 +1,21 @@
+import { gsap } from "gsap";
+
+const mainLoad = function() {
+    const mainContent = document.querySelector("#content");
+    mainContent.innerHTML = "";
+    const mainDiv = document.createElement("div");
+    mainDiv.classList.add("mainLoad");
+
+    const h2 = document.createElement("h2");
+    h2.textContent = "Sapore Italiano";
+
+    const p = document.createElement("p");
+    p.textContent = "Welcome to Sapore Italiano, where the warm ambiance of an authentic Italian trattoria meets the flavors of a traditional Italian kitchen. Our menu features classic dishes made with the freshest and most authentic ingredients, from handmade pasta to wood-fired pizzas and the finest wines from all regions of Italy.";
+    mainDiv.appendChild(h2);
+    mainDiv.appendChild(p);
+    mainContent.appendChild(mainDiv);
+    gsap.from(".mainLoad", {duration: 1, opacity: 0, y: 50})
+    console.log("module work!")
+}
+
+export default mainLoad;
