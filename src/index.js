b216e3c1d0c2afa94d6ccfa5cf64f9969e861025
viraldL <107942715+viraldL@users.noreply.github.com>
2023-01-11 19:57:34 +0100

    Start menu design

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import { gsap } from "gsap";
 import mainLoad from "./mainPage.js";
 
-const homeBtn = document.querySelector("#homeBtn");
+const load = (function(){
+    window.addEventListener("DOMContentLoaded", () => {
+        // mainLoad();
+    });
+})();
 
-homeBtn.addEventListener("click", () => {
-    console.log("work");
-    mainLoad();
-});
+const displayController = (function(){
+    const homeBtn = document.querySelector("#homeBtn");
+    const menuBtn = document.querySelector("#menuBtn");
+    const contactBtn = document.querySelector("#contactBtn");
+
+    homeBtn.addEventListener("click", () => {
+        console.log("witaj");
+    })
+})();
+
